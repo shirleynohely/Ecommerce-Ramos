4ddd558eb8721c9dc5c21cd53ba5514bbdb24592
Leyly <nohely.ramosderosas@gmail.com>
2022-06-06 10:47:01 -0300

    sicronizar counter

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 const CartWidget = () =>{
     return(
-        <nav class="navbar navbar-light bg-light">
-        <div class="container">
-          <img src="/images/cart.svg" alt="cart-widget"/><span>1</span>
-        </div>
+        <nav className="navbar navbar-light bg-light">
+        <Link to={`/cart`} className="container">
+          <img src="/images/cart.svg" alt="cart-widget"/><span></span>
+        </Link>
       </nav>
     );
 }
